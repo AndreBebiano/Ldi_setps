f96d8e7cd3e2add5da6e9555ba8d47a062fef510
AndreBebiano <andresilvabebiano@gmail.com>
2026-02-10 08:43:52 +0000

    adicionado css no index

--- index.php
+++ index.php
@@ -7,9 +7,11 @@
     <link rel="stylesheet" href="perso.css">
 </head>
 <body>
-    <div class="links">
-        <a href="letsdoit1/step1.php">Lets Do It 1</a> <br>
-        <a href="letsdoit2/carro.php">Lets Do It 2</a>
+    <div class="index">
+        <div class="links">
+            <a href="letsdoit1/step1.php">Lets Do It 1</a>
+            <a href="letsdoit2/carro.php">Lets Do It 2</a>
+        </div>
     </div>
 </body>
 </html>
